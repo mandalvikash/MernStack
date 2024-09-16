c1f832671a36670240a67ed4059c95d7b7acea56
mandalvikash <vikashmanda451@gmail.com>
2024-09-16 23:45:02 +0530

    class 2 added week 3

--- Week-3/class-2-Database-Authentication/1.0-fetchAPI.js
+++
@@ -1,7 +0,0 @@
-function  getAnimalData(){
-  fetch("https://fakerapi.it/api/v1/persons").then(function(response){
-    response.json().then(function(finalData){
-      console.log(finalData);
-    })
-  })
-}
