27e5753e7f8c43aa142c35940a77b351905faec4
mandalvikash <vikashmanda451@gmail.com>
2024-09-17 09:40:38 +0530

    Add new feature X

--- Week-3/class-2-Database-and-Authentication/1.0-fetchAPI.js
+++ Week-3/class-2-Database-and-Authentication/1.0-fetchAPI.js
@@ -1,13 +1,12 @@
-// function getAnimalData(){
-//     fetch("https://fakerapi.it/api/v1/persons").then(function(response){
-//         response.json().then(function(finalData){
-//             console.log(finalData);
-//         })
-//     })
-// }
-async function getAnimalData() {
-    const response=await fetch("https://fakerapi.it/api/v1/persons");
-    const finalData=await response.json();
-    console.log(finalData)
-    
+function getAnimalData(){
+    fetch("https://fakerapi.it/api/v1/persons").then(async function(response){
+            const data=await response.json();
+            document.getElementById("container").innerHTML=JSON.stringify(data);
+    })
 }
+// async function getAnimalData() {
+//     const response=await fetch("https://fakerapi.it/api/v1/persons");
+//     const finalData=await response.json();
+//     console.log(finalData)
+    
+// }
